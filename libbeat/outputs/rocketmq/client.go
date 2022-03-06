78a489091451ea78002b753d975631926506dfb6
caigy <csgytsai@163.com>
2022-03-06 14:40:56 +0800

    add license

--- libbeat/outputs/rocketmq/client.go
+++ libbeat/outputs/rocketmq/client.go
@@ -1,3 +1,18 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package rocketmq
 
 import (
@@ -29,8 +44,7 @@ type client struct {
 }
 
 func (c *client) Connect() error {
-	c.log.Warn("Enter Connect()..............")
-	c.log.Warnf("connect: %v", c.namesrvAddr)
+	c.log.Warnf("connecting: %v", c.namesrvAddr)
 
 	p, err := rocketmq.NewProducer(
 		producer.WithNsResolver(primitive.NewPassthroughResolver(c.namesrvAddr)),
@@ -54,9 +68,8 @@ func (c *client) Connect() error {
 	return nil
 }
 
-//关闭触发函数
 func (c *client) Close() error {
-	c.log.Warn("Enter Close()...............")
+	c.log.Warn("Enter Close(), shutdown...")
 	if c.producer != nil {
 		c.producer.Shutdown()
 		c.producer = nil
@@ -65,9 +78,7 @@ func (c *client) Close() error {
 	return nil
 }
 
-//有新的日志信息产生，会触发该函数
 func (c *client) Publish(_ context.Context, batch publisher.Batch) error {
-	c.log.Warn("Enter Publish()..........")
 
 	defer batch.ACK()
 	st := c.observer
@@ -84,16 +95,10 @@ func (c *client) Publish(_ context.Context, batch publisher.Batch) error {
 		}
 	}
 
-	// batch.ACK()
-	// st.Dropped(dropped)
-	// st.Acked(len(events) - dropped)
-
 	return nil
 }
 
 func (c *client) publishEvent(event *publisher.Event) bool {
-	c.log.Warn("Enter publishEvent().......................")
-
 	serializedEvent, err := c.codec.Encode(c.index, &event.Content)
 	if err != nil {
 		c.observer.Dropped(1)
@@ -102,15 +107,13 @@ func (c *client) publishEvent(event *publisher.Event) bool {
 			return false
 		}
 		c.log.Errorf("Unable to encode event: %v", err)
-		c.log.Debugf("Failed event: %v", event)
 		return false
 	}
 
 	c.observer.WriteBytes(len(serializedEvent) + 1)
 
-	//新增日志内容
-	str := string(serializedEvent)
-	c.log.Warnf("Processing event: %v", str)
+	// str := string(serializedEvent)
+	// c.log.Warnf("Processing event: %v", str)
 
 	buf := make([]byte, len(serializedEvent))
 	copy(buf, serializedEvent)
